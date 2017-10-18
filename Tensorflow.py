094f11c7a397637dff8b207d9a9b8ef731f9373f
zjy1990 <jeremy.zhujy@gmail.com>
2017-10-18 11:25:42 -0700

    commit

--- Tensorflow.py
+++ Tensorflow.py
@@ -3,32 +3,33 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
+#params
+batch_size = 30
+num_per_batch = 10
+num_class = 2
+lstm_size = 128
+num_iteration = 100
 
-stock_data = pd.DataFrame(data=np.random.randn(1000)*0.1)
-labels_data = pd.DataFrame(data=np.zeros((1000,2)))
+#fake data
+stock_data = pd.DataFrame(data=np.random.randn(batch_size*num_iteration, num_per_batch)*0.1)
+labels_data = pd.DataFrame(data=np.zeros((batch_size*num_iteration,num_class)))
 labels_data.loc[:,0] = np.int_(stock_data.loc[:,0]>=0)
 labels_data.loc[:,1] = np.int_(stock_data.loc[:,0]<0)
 
 
-#params
-batch_size = 10
-num_class = 2
-lstm_size = 64
-num_interation = 100
-
 #LSTM tensorflow models
 labels = tf.placeholder(tf.float32,[batch_size,num_class])
-input_data = tf.placeholder(tf.float32,[1,batch_size,1])
-
+input_data = tf.placeholder(tf.float32,[batch_size,num_per_batch,1])
+input_data = tf.unstack(input_data,num_per_batch,1)
 lstmCell = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
 lstmCell = tf.nn.rnn_cell.DropoutWrapper(cell=lstmCell,output_keep_prob=0.85)
 
-value,_ = tf.nn.dynamic_rnn(lstmCell,input_data,dtype=tf.float32)
+value,_ = tf.nn.static_rnn(lstmCell,input_data,dtype=tf.float32)
 weight = tf.Variable(tf.truncated_normal([lstm_size,num_class]))
-bias = tf.Variable(tf.constant(0.1,shape=[batch_size,num_class]))
-
+bias = tf.Variable(tf.constant(0.1,shape=[num_class]))
+value = tf.stack(value)
 last = tf.gather(value,int(value.get_shape()[0])-1)
-prediction = (tf.matmul(last,weight)+bias)
+prediction = (tf.matmul(last, weight)+bias)
 
 
 correct_prediction = tf.equal(tf.argmax(prediction,1),tf.argmax(labels,1))
