1f886020b57360f63585c64bf5a0dc7d757592fc
Jeremy <Jeremy@Jeremy-Macbook.local>
2017-10-19 21:16:46 -0700

    new commits

--- Tensorflow.py
+++ Tensorflow.py
@@ -10,47 +10,34 @@ num_class = 2
 lstm_size = 64
 num_iteration = 1000
 display_step = 100
-#fake data
 
+#fake data
 stock_data = (np.random.randn(batch_size*num_iteration, num_per_batch,1)*0.1).tolist()
 labels_data = pd.DataFrame(data=np.zeros((batch_size*num_iteration,num_class)))
 predict_stock_data = pd.DataFrame(np.random.randn(batch_size*num_iteration,1))
 labels_data.loc[:,0] = np.int_(predict_stock_data.loc[:,0]>=0)
 labels_data.loc[:,1] = np.int_(predict_stock_data.loc[:,0]<0)
 
-
+#define weight and bias
 weight = tf.Variable(tf.truncated_normal([lstm_size,num_class]))
 bias = tf.Variable(tf.constant(0.1,shape=[num_class]))
-
+#define labels and input data format
 labels = tf.placeholder(tf.float32,[batch_size,num_class])
 input_data = tf.placeholder(tf.float32,[batch_size,num_per_batch,1])
 
-
+#LSTM cell construction
 def LSTM(input_data,weight,bias):
     input_data = tf.unstack(input_data, num_per_batch, 1)
     lstmCell = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
     lstmCell = tf.nn.rnn_cell.DropoutWrapper(cell=lstmCell, output_keep_prob=0.85)
     value, _ = tf.nn.static_rnn(lstmCell, input_data, dtype=tf.float32)
     value = tf.stack(value)
-    last = tf.gather(value, int(value.get_shape()[0]) - 1)
+    last = tf.gather(value, int(value.get_shape()[0]) - 1) #take the last one
     prediction = (tf.matmul(last, weight) + bias)
     return prediction
 
 prediction = LSTM(input_data,weight,bias)
-#LSTM tensorflow models
-
-#input_data = tf.unstack(input_data,num_per_batch,1)
-
-
-
-
-
-
-
-#value = tf.transpose(value,[1,0,2])
-
-
-
+#define cost
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels = labels))
 optimizer = tf.train.AdamOptimizer(1e-4).minimize(cost)
 correct_prediction = tf.equal(tf.argmax(prediction,1),tf.argmax(labels,1))
@@ -77,7 +64,7 @@ with tf.Session() as sess:
         nextBatchLabels = labels_data.loc[step*batch_size:(step+1)*batch_size-1,]
         #nextBatch = tf.unstack(nextBatch)
         sess.run(optimizer,feed_dict= {input_data: nextBatch,labels: nextBatchLabels})
-        if step % display_step == 0 or step == 1:
+        if step % display_step == 0 or step == 1:#report summary
             # Calculate batch accuracy & loss
             acc, loss = sess.run([accuracy, cost], feed_dict={input_data: nextBatch, labels: nextBatchLabels})
             print("Step " + str(step * batch_size) + ", Minibatch Loss= " + \
