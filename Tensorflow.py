1a295d3eeff3a4ca6c309eb4d2ee99cf647d512e
zjy1990 <jeremy.zhujy@gmail.com>
2017-10-29 00:36:50 -0700

    commits

--- Tensorflow.py
+++ Tensorflow.py
@@ -24,12 +24,13 @@ lstm_size = 64
 num_iteration = train_data.shape[0] - batch_size + 1
 display_step = batch_size
 #strategy params
-target_buy = 0.004
-target_sell = -0.008
-trans_cost = 0.002
+target_buy = 0.003
+target_sell = -0.003
+trans_cost = 0.001
 borrow_rate = 0.0002
+initial_capital = 100
 ptf_value = []
-ptf_value.append(100)
+ptf_value.append(initial_capital)
 
 
 #function to select batch data for random draw
@@ -73,14 +74,35 @@ def getTestingBatch_timeseries(batch_size, testdata):
 
     return(testBatch,testLabel,real_return)
 
-def getReturn(action,realize_return):
+def getReturn(net_position,action,realize_return):
+
     if action == "Buy":
-        value = realize_return - trans_cost
-    elif action == "Hold":
-        value = 0
+        if net_position == 0:
+            Tcost = trans_cost
+        elif net_position == 1:
+            Tcost = 0
+        else:
+            Tcost = trans_cost*2
+        net_position = 1
+        adj_ret = 1 + realize_return - Tcost
+    elif action == "Sell":
+        if net_position == 0:
+            Tcost = trans_cost + borrow_rate
+        elif net_position == 1:
+            Tcost = 2*trans_cost + borrow_rate
+        else:
+            Tcost = 0
+        net_position = -1
+        adj_ret = 1 - realize_return - Tcost
     else:
-        value = -realize_return - trans_cost - borrow_rate
-    return(value)
+        if net_position == -1:
+            Tcost = borrow_rate
+            adj_ret = 1 - realize_return - Tcost
+        else:
+            Tcost = 0
+            adj_ret = 1 + realize_return - Tcost
+
+    return(adj_ret,net_position)
 
 #define weight and bias
 weight = tf.Variable(tf.truncated_normal([lstm_size,num_class]))
@@ -130,8 +152,10 @@ with tf.Session() as sess:
 
     print("Optimization Finished!")
 
+    net_position = 0 #initialize action
     #testing data
     for step in range(test_data.shape[0]):
+
         nextTestBatch,nextTestBatchLabels,realize_return = getTestingBatch_timeseries(batch_size, test_data.iloc[step,:])
         #nextBatch = tf.unstack(nextBatch)
         sess.run(optimizer, feed_dict={input_data: nextTestBatch, labels: nextTestBatchLabels})
@@ -144,12 +168,15 @@ with tf.Session() as sess:
         else:
             action = "Sell"
         date = test_data.iloc[step,0]
-        ptf_value.append((1+getReturn(action,realize_return))*ptf_value[step])
+        adj_ret,net_position = getReturn(net_position,action,realize_return)
+        ptf_value.append(adj_ret*ptf_value[step])
         print(str(date) +" " + action +" : Cumulative portfolio value = " + str(ptf_value[step+1]))
+
     print("Testing Finished!")
 
-
+benchmark = initial_capital*np.cumprod(1+test_data.iloc[:, 1])
 plt.plot(ptf_value)
+plt.plot(benchmark)
 plt.ylabel('Cumulative portfolio value $')
 plt.xlabel('Time')
 plt.show()
