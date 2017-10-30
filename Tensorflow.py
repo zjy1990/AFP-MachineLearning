6e671d9238197ff827c5ac966193a5b0a0ccb4d0
zjy1990 <jeremy.zhujy@gmail.com>
2017-10-29 23:15:05 -0700

    commits

--- Tensorflow.py
+++ Tensorflow.py
@@ -3,54 +3,115 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+#import raw data
 
 
+#financial
+#train_data = pd.read_csv('data/fin_stock.csv',sep = ',')
+#test_data = pd.read_csv('data/test_fin.csv',sep = ',')
+#Index
+raw_data = pd.read_csv('data/index_data.csv',sep = ',')
 
+train_data = raw_data.iloc[0:4729,]
+test_data = raw_data.iloc[4729:4989,]
+#tech firm
+# train_data = pd.read_csv('data/tech_stock.csv',sep = ',')
+# test_data = pd.read_csv('data/test_tech.csv',sep = ',')
 
 #params
-batch_size = 500
-num_per_batch = 7
-num_class = 2
+batch_size = 100
+num_per_batch = 30
+num_per_day = train_data.shape[1] - 2
+num_class = 3
 lstm_size = 64
-num_iteration = 10000
-display_step = 500
+num_iteration = 5000
+#num_iteration = train_data.shape[0] - batch_size + 1
+display_step = batch_size
+#strategy params
+target_buy = 0.003
+target_sell = -0.003
+trans_cost = 0.0005
+borrow_rate = 0.0002
+initial_capital = 100
+ptf_value = []
+ptf_value.append(initial_capital)
+ptf_ret = []
 
-#function to select batch data
-def getTrainingBatch(batch_size):
-    maxNumber = stock_data.shape[0]
+#function to select batch data for random draw
+def getTrainingBatch_random(batch_size, traindata):
+    maxNumber = traindata.shape[0]-num_per_batch-1
     batchIndex = np.random.randint(0, maxNumber, batch_size)
-    trainBatch = stock_data.loc[batchIndex, :]
+    trainBatch = np.ndarray((batch_size, num_per_batch, num_per_day))
     trainLabel = pd.DataFrame(data=np.zeros((batch_size, num_class)))
-    trainLabel.loc[:, 0] = np.int_(trainBatch.loc[:, '^GSPC'] >= 0)
-    trainLabel.loc[:, 1] = np.int_(trainBatch.loc[:, '^GSPC'] < 0)
-    trainBatch = trainBatch.iloc[:, 2:trainBatch.shape[1]]
-    trainBatch = np.array(trainBatch)
-    trainBatch = np.reshape(trainBatch,(batch_size,num_per_batch,1)).tolist()
+
+    for i in range(len(batchIndex)):
+        trainBatch[i,] = traindata.iloc[batchIndex[i]:(batchIndex[i]+num_per_batch),2:traindata.shape[1]]
+        trainLabel.loc[i, 0] = np.int(traindata.iloc[(batchIndex[i]+num_per_batch+1), 1] >= target_buy)
+        trainLabel.loc[i, 1] = np.int((traindata.iloc[(batchIndex[i]+num_per_batch+1), 1] < target_buy) & (traindata.iloc[(batchIndex[i]+num_per_batch+1), 1] >= target_sell))
+        trainLabel.loc[i, 2] = np.int(traindata.iloc[(batchIndex[i]+num_per_batch+1), 1] < target_sell)
+
+    trainBatch = trainBatch.tolist()
     return(trainBatch,trainLabel)
 
 
-##get training and testing batch
-#fake data
-#datastock_data = (np.random.randn(batch_size*num_iteration, num_per_batch,1)*0.1).tolist()
-# labels_data = pd.DataFrame(data=np.zeros((batch_size*num_iteration,num_class)))
-# predict_stock_data = pd.DataFrame(np.random.randn(batch_size*num_iteration,1))
-# labels_data.loc[:,0] = np.int_(predict_stock_data.loc[:,0]>=0)
-# labels_data.loc[:,1] = np.int_(predict_stock_data.loc[:,0]<0)
+def getTestingBatch_timeseries(batch_size, testdata):
+    real_return = testdata.iloc[num_per_batch, 1]
+    testLabel = pd.DataFrame(data=np.zeros((batch_size, num_class)))
 
-#real data
-stock_data = pd.read_csv('/Users/peipeidew/Desktop/AFP/LSTM/code/IDX.csv',sep = ',')
+    testBatch = np.ndarray((batch_size, num_per_batch, num_per_day))
 
+    for i in range(batch_size):
+        testBatch[i,] = testdata.iloc[0:num_per_batch,2:traindata.shape[1]]
+        testLabel.loc[i, 0] = np.int(real_return >= target_buy)
+        testLabel.loc[i, 1] = np.int((real_return < target_buy) & (real_return >= target_sell))
+        testLabel.loc[i, 2] = np.int(real_return < target_sell)
+
+    testBatch = testBatch.tolist()
+
+    return(testBatch,testLabel,real_return)
+
+def getReturn(net_position, action, actual_return):
+
+    if action == "Buy":
+        if net_position == 0:
+            Tcost = trans_cost
+        elif net_position == 1:
+            Tcost = 0
+        else:
+            Tcost = trans_cost*2
+        net_position = 1
+        adj_ret = 1 + actual_return - Tcost
+
+    elif action == "Sell":
+        if net_position == 0:
+            Tcost = trans_cost + borrow_rate
+        elif net_position == 1:
+            Tcost = 2*trans_cost + borrow_rate
+        else:
+            Tcost = borrow_rate
+        net_position = -1
+        adj_ret = 1 - actual_return - Tcost
+    else:
+        if net_position == -1:
+            Tcost = borrow_rate
+            adj_ret = 1 - actual_return - Tcost
+        else:
+            Tcost = 0
+            adj_ret = 1 + actual_return - Tcost
+
+    return(adj_ret,net_position)
 
 #define weight and bias
 weight = tf.Variable(tf.truncated_normal([lstm_size,num_class]))
 bias = tf.Variable(tf.constant(0.1,shape=[num_class]))
 #define labels and input data format
 labels = tf.placeholder(tf.float32,[batch_size,num_class])
-input_data = tf.placeholder(tf.float32,[batch_size,num_per_batch,1])
+input_data = tf.placeholder(tf.float32,[batch_size,num_per_batch,num_per_day])
 
 #LSTM cell construction
 def LSTM(input_data,weight,bias):
-    input_data = tf.unstack(input_data, num_per_batch, 1)
+    input_data = tf.unstack(input_data, axis = 1)
     lstmCell = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
     lstmCell = tf.nn.rnn_cell.DropoutWrapper(cell=lstmCell, output_keep_prob=0.85)
     value, _ = tf.nn.static_rnn(lstmCell, input_data, dtype=tf.float32)
@@ -64,34 +125,74 @@ prediction = LSTM(input_data,weight,bias)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels = labels))
 optimizer = tf.train.AdamOptimizer(1e-4).minimize(cost)
 correct_prediction = tf.equal(tf.argmax(prediction,1),tf.argmax(labels,1))
+prediction_results = tf.argmax(prediction,1)[0]
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
-
     sess.run(init)
-    #save summary
-    # tf.summary.scalar('Loss',loss)
-    # tf.summary.scalar('Accuracy',accuracy)
-    # tf.summary.histogram('Weight',weight)
-    # tf.summary.histogram('Bias',bias)
-    # merged_summary = tf.summary.merge_all()
-    # logdir = "~/Desktop"
-    # writer = tf.summary.FileWriter(logdir)
-    # writer.add_graph(sess.graph)
 
-#run model
+
+#run model random time series
+    print("Optimization Starts!")
     for step in range(num_iteration):
-        nextBatch,nextBatchLabels = getTrainingBatch(batch_size)
+        #traindata = train_data.iloc[step:step+batch_size,:]
+        traindata =train_data
+        nextTrainBatch,nextTrainBatchLabels = getTrainingBatch_random(batch_size,traindata)
         #nextBatch = tf.unstack(nextBatch)
-        sess.run(optimizer,feed_dict= {input_data: nextBatch,labels: nextBatchLabels})
-        if step % display_step == 0 or step == 1:#report summary
+        sess.run(optimizer,feed_dict= {input_data: nextTrainBatch,labels: nextTrainBatchLabels})
+        if step % display_step == 0:#report summary
             # Calculate batch accuracy & loss
-            acc, loss = sess.run([accuracy, cost], feed_dict={input_data: nextBatch, labels: nextBatchLabels})
-            print("Step " + str(step * batch_size) + ", Minibatch Loss= " + \
+            acc, loss = sess.run([accuracy, cost], feed_dict={input_data: nextTrainBatch, labels: nextTrainBatchLabels})
+            print("Step " + str(step) + ", Minibatch Loss= " + \
                   "{:.6f}".format(loss) + ", Training Accuracy= " + \
                   "{:.5f}".format(acc))
 
     print("Optimization Finished!")
 
+    net_position = 0 #initialize action
+    # #testing data
+    for step in range(test_data.shape[0] - num_per_batch - 1):
+
+        nextTestBatch,nextTestBatchLabels,realize_return = getTestingBatch_timeseries(batch_size, test_data.iloc[step : step + num_per_batch + 1,:])
+        #nextBatch = tf.unstack(nextBatch)
+        sess.run(optimizer, feed_dict={input_data: nextTestBatch, labels: nextTestBatchLabels})
+
+        pred_result = sess.run(prediction_results, feed_dict={input_data: nextTestBatch, labels:nextTestBatchLabels})
+
+        if pred_result == 0:
+            action = "Buy"
+        elif pred_result == 1:
+            action = "Hold"
+        else:
+            action = "Sell"
+        date = test_data.iloc[step,0]
+        adj_ret,net_position = getReturn(net_position,action,realize_return)
+        ptf_value.append(adj_ret*ptf_value[step])
+        ptf_ret.append(adj_ret-1)
+        print(str(date) +" " + action +" : Cumulative portfolio value = " + str(ptf_value[step+1]))
+
+        # if step == test_data.shape[0]-1:#report summary
+        #     # Calculate batch accuracy & loss
+        #     acc, loss = sess.run([accuracy, cost], feed_dict={input_data: nextTestBatch, labels: nextTestBatchLabels})
+        #     print("Step " + str(step) + ", Minibatch Loss= " + \
+        #           "{:.6f}".format(loss) + ", Training Accuracy= " + \
+        #           "{:.5f}".format(acc))
+
+    print("Testing Finished!")
+
+
+
+benchmark = (initial_capital*np.cumprod(1+test_data.iloc[num_per_batch:test_data.shape[0], 1])).tolist()
+SR_ptf = np.average(ptf_ret)/np.std(ptf_ret)*np.sqrt(252)
+SR_mkt = np.average(test_data.iloc[num_per_batch:test_data.shape[0], 1])/np.std(test_data.iloc[num_per_batch:test_data.shape[0], 1])*np.sqrt(252)
+print("Portfolio sharpe ratio = "+ str(SR_ptf))
+print("Market sharpe ratio = "+ str(SR_mkt))
+plt.plot(ptf_value,'-b',label = 'Portfolio')
+plt.plot(benchmark,'-r',label = 'Benchmark')
+plt.axis([0,250,0,150])
+plt.ylabel('Cumulative portfolio value')
+plt.xlabel('Time')
+plt.legend()
+plt.show()
